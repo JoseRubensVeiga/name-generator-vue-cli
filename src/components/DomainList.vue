eee8914b142f8bfe57152f8df6d09c85e7e6c93b
José <jose.rubens.veiga@gmail.com>
2019-11-08 07:59:11 -0300

    create components

---
+++ src/components/DomainList.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <div class="container py-3">
+            <div class="row">
+                <div class="col-md-6 my-3">
+                    <ItemList v-bind:items="prefixes" itemName="Prefixo" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></ItemList>
+                </div>
+                <div class="col-md-6 my-3">
+                    <ItemList v-bind:items="sufixes" itemName="Sufixo" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></ItemList>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col">
+                    <h5>
+                        Nomes Disponíveis
+                        <span class="badge badge-info">{{ domains.length }}</span>
+                    </h5>
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item" v-for="(domain, i) in domains" v-bind:key="i">
+                                    {{ domain.name }}
+                                    <a v-bind:href="domain.checkout" class="btn btn-info float-right" role="button"><span class="fa fa-shopping-cart"></span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import ItemList from "./ItemList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+export default {
+  name: "app",
+  components: {
+    ItemList
+  },
+  data: function() {
+    return {
+      prefix: null,
+      sufix: null,
+      prefixes: ["Air", "Jet", "Flight"],
+      sufixes: ["Hub", "Station", "Smart"],
+    };
+  },
+  methods: {
+    addPrefix(prefix) {
+      this.prefixes.push(prefix);
+      this.prefix = "";
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+      this.sufix = "";
+    },
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+      this.prefix = "";
+    },
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+      this.sufix = "";
+    }
+  },
+  computed: {
+    domains() {
+      let domains = [];
+
+      for(let p of this.prefixes) {
+        for(let s of this.sufixes) {
+          const name = `${p}${s}`;
+          const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${name.toLowerCase()}&tld=.com.br`;
+          domains.push({
+            name,
+            checkout
+          });
+        }
+      }
+
+      return domains;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+
---
+++ src/components/ItemList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h5>
+        {{ itemName }}s
+        <span class="badge badge-info">{{ items.length }}</span>
+      </h5>
+      <div class="card">
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="(item, i) in items" v-bind:key="i">
+              {{ item }}
+              <button class="btn btn-danger float-right" v-on:click="deleteItem(item)"><span class="fa fa-trash"></span></button>
+            </li>
+          </ul>
+          <br />
+          <div class="input-group">
+            <input type="text" class="form-control" v-on:keyup.enter="addItem(item)" v-model="item" v-bind:placeholder="`Digite o ${itemName}`" />
+            <div class="input-group-append">
+              <button class="btn btn-info" v-on:click="addItem(item)">
+                <span class="fa fa-plus"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  props: [
+    "items",
+    "itemName"
+  ],
+  data() {
+    return {
+      item: ""
+    };
+  },
+  methods: {
+    addItem(item) {
+      this.$emit("addItem", item);
+      this.item = "";
+    },
+    deleteItem(item) {
+      this.$emit("deleteItem", item);
+      this.item = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
